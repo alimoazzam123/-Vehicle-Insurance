499034dc64dc892c2bb71a85794bb8a4470f392b
Moazzam <mdmoazzamali984@gmail.com>
2025-03-26 16:11:23 +0530

    Model Created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Md Moazzam Ali",
+    author_email="mdmoazzamali984@gmail.com",
+    packages=find_packages()
+)
